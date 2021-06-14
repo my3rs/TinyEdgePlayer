a3da545b67c53b571e0485d8d497c92f3161dea9
seaHi <me@seahi.me>
2021-06-14 23:47:39 +0800

    feat(task): 增加生成任务请求的公共接口 同时用glog替换easylogging++

--- Task.h
+++ Task.h
@@ -2,13 +2,34 @@
 #define TINYEDGEPLAYER_TASK_H
 
 
+/*
+ * 任务请求类和生成“符合不同分布特征的任务请求”函数
+ */
+
+#include <random>
+#include <chrono>
+
 struct Task
 {
-    int time;
-    int storage;
+    int time;       // 计算开销，单位为ms
+    int storage;    // 存储开销，单位为MB
 
     Task(int t, int s) : time(t), storage(s) {}
 };
 
+/*
+ * 生成随机的任务时间，单位为ms
+ */
+int GenerateRandomTime();
+
+/*
+ * 生成随机的任务存储开销，单位为MB
+ */
+int GenerateRandomStorage();
+
+/*
+ * 生成随机的任务请求
+ */
+Task GenerateRandomTask();
 
 #endif //TINYEDGEPLAYER_TASK_H
