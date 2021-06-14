e30a328745efb69fd44d936272e33e3839d36337
seaHi <me@seahi.me>
2021-06-14 23:48:03 +0800

    feat(task): 增加生成任务请求的公共接口 同时用glog替换easylogging++

--- Task.cpp
+++ Task.cpp
@@ -1,5 +1,25 @@
-//
-// Created by seaHi on 2021/5/17.
-//
-
 #include "Task.h"
+
+
+int GenerateRandomTime()
+{
+    auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine e(seed);
+    std::uniform_int_distribution u(50, 150);
+
+    return u(e);
+}
+
+int GenerateRandomStorage()
+{
+    auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine e(seed);
+    std::uniform_int_distribution u(10, 50);
+
+    return u(e);
+}
+
+Task GenerateRandomTask()
+{
+    return Task(GenerateRandomTime(), GenerateRandomStorage());
+}
