081ca0177bc36f775865b3838e37da3791ae2467
seaHi <me@seahi.me>
2021-06-18 00:57:43 +0800

    chore(server): 给注释多加一个星号

--- Server.h
+++ Server.h
@@ -18,38 +18,38 @@ public:
 
     Server(int cpu, int ram, int id);
 
-    /*
+    /**
      * 停止Server
      */
     void Stop();
 
-    /*
+    /**
      * 执行一个Task
      */
     auto Execute(Task t) -> std::future<bool>;
 
 public:
-    /*
+    /**
      * 获得服务器ID
      */
     int     GetId() { return id_; }
 
-    /*
+    /**
      * 获得CPU负载
      */
     double  GetCpuLoad() { return cpu_.load(); }
 
-    /*
+    /**
      * 获得近期的平均任务队列长度
      */
     int     GetTaskQueueSize() { return cpu_.GetTaskQueueSize(); }
 
-    /*
+    /**
      * 获得近期的处理速度
      */
     double  GetCurrentSpeed() { return cpu_.GetCurrentSpeed_(); }
 
-    /*
+    /**
      * 打印日志
      */
     void    PrintStatus();
@@ -67,19 +67,22 @@ private:
     RateLimiter rate_limiter_;  // 限流器
 
 private:
-    /*
+    /**
      * 构造日志字符串并返回
      */
     std::string     GetStatusLogString_();
 
-    /*
+    /**
      * 控制本地资源管理的线程函数
      * 不需要增加单独的线程，由cpu_来执行它即可
      */
     void    GcFunc();
 };
 
-
+/**
+ * 返回一个Server的share_ptr指针
+ * @id 必需指定服务器ID
+ */
 std::shared_ptr<Server> CreateOneServer(int id);
 
 
