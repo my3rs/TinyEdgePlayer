e9d6bf94756b7ddbb281b09ff1f9bccea4019e97
seaHi <me@seahi.me>
2021-06-18 00:28:38 +0800

    chore(config): 将几个参数从程序中独立出来

--- config.h
+++ config.h
@@ -20,23 +20,29 @@ namespace Config
     const unsigned kGameTerm = 1;    // 单位 s
     const unsigned kLatencyThreshold = 80; // 单位 ms
 
-    // 任务的最短耗时
-    const unsigned kMinRequestTime = 80;   // 单位 ms
-    // 任务的最小存储占用
-    const unsigned kMinRequestStorage = 80; // MB
+    // 任务的最短和最长耗时，单位 ms
+    const unsigned kMinRequestTime = 50;
+    const unsigned kMaxRequestTime = 150;
+
+    // 任务的最小和最大存储占用，单位 MB
+    const unsigned kMinRequestStorage = 10;
+    const unsigned kMaxRequestStorage = 50;
 
     // proxy 节点获取服务器权重的周期，单位 s
     const unsigned kProxyUpdateServerWeightTime = kGameTerm;
 
-    // 内存做一次 GC 的时间，ms
+    // 做一次本地资源管理的时间，ms
     const unsigned kGcTime = 100;
-    // 一次 GC 的释放大小，MB
+    // 一次本地资源管理的释放空间大小，MB
     const unsigned kGcSize = 30;
-    // 默认的 GC 间隔，ms
-    const unsigned kGcInter = 1000;
+    // 默认的本地资源管理时间间隔，ms
+    const unsigned kGcInterval = 1000;
 
     // 默认的限流参数，单位 QPS
     const unsigned kDefaultRateLimit = 60;
+
+    // 客户端发送请求的时间时隔，单位 ms
+    const unsigned kRequestInterval = 20;
 }
 
 #endif //EDGEPLAYER_CONFIG_H
